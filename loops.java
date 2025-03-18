2bcd93c8f313ef268ea8cf41d2e22855c431685a
Jayden-Slane <jaydenslane123@gmail.com>
2025-03-18 10:26:53 -0400

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class loops {
+    public static void main (String[]args){
+
+        Scanner input = new Scanner(System.in);
+        int response;
+    do {
+        System.out.print("Enter 1 or 0:");
+        response = input.nextInt();
+    } while (response == 1);
+    
+    
+
+
+    }
+    
+}
