03b1a3d63cb3a5087a2650b4e23a4c57db53058e
Jayden-Slane <jaydenslane123@gmail.com>
2025-03-19 10:26:00 -0400

    a

---
+++ Prompter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Prompter {
+    public static void main (String[]args){
+
+        int number;
+        int number2;
+
+
+Scanner input = new Scanner(System.in);
+System.out.print("Enter max: ");
+number = input.nextInt();
+System.out.print("Enter min: ");
+number = input.nextInt();
+
+do{
+    System.out.println("Enter a number between min and max: ");
+}
+
+
+    }
+    
+}
