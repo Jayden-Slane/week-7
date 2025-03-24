69682112aeede89a9b589845cf96b449bc16be21
Jayden-Slane <jaydenslane123@gmail.com>
2025-03-24 09:56:24 -0400

    a

---
+++ oddsum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class oddsum {
+    public static void main (String[]args){
+
+        int number;
+        Scanner imput = new Scanner (System.in);
+        System.out.print("Enter a number: ");
+        number = imput.nextInt();
+        int sum = 0;
+        for (int i = 1; i<=number; i += 2){
+            sum += i;
+
+
+        }
+
+
+
+    }
+    
+}
